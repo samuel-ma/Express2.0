ae0978c5575f33610da56680611a686e22c9ebaa
samuel-ma <mabilmakur@gmail.com>
2023-03-03 16:30:30 +0200

    More commits

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var post = require("./routes/post");
+
+var bodyParser = require("body-parser");
+var cookie = require("cookie-parser");
 
 var app = express();
 
@@ -19,6 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use the router module 
+app.use("/blog", post);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -85,6 +91,61 @@ app.get(/.*fly$/, function(req,res){
   res.send("/.*fly$/");
 })
 
+// working with middleware
+app.get("/api/v1/stories/:id", function(res,req,next){
+  // carryout authorization
+  return next();
+}), function(req,res,next){
+  // extract user id and fetch the database
+  req.story = story;
+  return next();
+}, function(req,res){
+  // output the result of the database search
+  res.send(res.story);
+}
+
+// simple request time logger
+const myLogger = function(req,res,next){
+  console.log("A new request received at " + Date.now());
+  next();
+}
+app.use(myLogger);
+
+app.get("./routes/users.js", function(req,res){
+  res.send("Hello World!");
+})
+
+// we can alaso pass callbacks as items of an array thanks to arguments
+const authAdmin = function(req,res,next){
+  // ...
+  return next();
+}
+
+const getUsers = function(req,res,next){
+  // ...
+  return next();
+}
+
+const renderUsers = function(req,res,next){
+  // ...
+  return next();
+}
+
+const admin = [authAdmin, getUsers, renderUsers];
+app.get("/admin", admin);
+
+// chained route handlers that are defined by using app.route()
+app.route("/book")
+  .get(function(req, res){
+    res.send("Get a random book");
+  })
+  .post(function(req,res){
+    res.send("Add a book");
+  })
+  .put(function(req,res){
+    res.send("Update a book");
+  })
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -95,5 +156,21 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Error handling middleware
+app.use(function(err,req,res,next){
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+})
+
+app.get("/", function(req,res,next){
+  next(new Error("I am passing you an error!"));
+})
+
+// parse json data
+app.use(bodyParser.json());
+
+// parse cookies
+app.use(cookie());
 
 module.exports = app;
